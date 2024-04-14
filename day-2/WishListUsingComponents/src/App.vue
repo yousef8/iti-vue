b387faa169dff835871f24b098ff87f3fa641bf2
Yousef Elsayed <yousef4511@outlook.com>
2024-04-14 14:49:07 +0200

    Setup WishList project [day 2]

---
+++ day-2/WishListUsingComponents/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- App -->
+  <div class="container-lg p-4 d-flex flex-wrap justify-content-around">
+    <book-card :book="book" v-for="book in books" :key="book.id"></book-card>
+  </div>
+</template>
+
+<script>
+import books from './assets/books.json'
+import BookCard from './components/BookCard.vue'
+
+export default {
+  components: { BookCard },
+  data() {
+    return {
+      books: books
+    }
+  }
+}
+</script>
---
+++ day-2/WishListUsingComponents/src/components/BookCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="'/src/assets/' + book.imageLink" class="card-img-top" alt="book cover" />
+    <div class="card-body">
+      <h5 class="card-title">{{ book.title }}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"><b>Author : </b> {{ book.author }}</li>
+      <li :class="[book.pages < 300 ? 'less' : 'more', 'list-group-item']">
+        <b>No. Pages :</b> {{ book.pages }}
+      </li>
+      <li class="list-group-item"><b>Price : </b> {{ book.price }}</li>
+    </ul>
+    <!-- <div class="card-body">
+      <a class="card-link" @click="addToWishList(book)" v-if="!isBookInWishList(book)"
+        >Add to WishList</a
+      >
+      <a class="card-link" v-else>Added to wish list</a>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    book: {
+      author: String,
+      country: String,
+      imageLink: String,
+      language: String,
+      link: String,
+      pages: Number,
+      title: String,
+      year: Number,
+      id: Number,
+      price: Number,
+      required: true
+    }
+  }
+}
+</script>
