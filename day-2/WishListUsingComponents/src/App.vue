e03be104bd29e8298c654942a9b6b40ebad4d955
Yousef Elsayed <yousef4511@outlook.com>
2024-04-14 16:36:15 +0200

    Add NavBar

--- day-2/WishListUsingComponents/src/App.vue
+++ day-2/WishListUsingComponents/src/App.vue
@@ -1,19 +1,36 @@
 <template>
+  <nav-bar :showLibrary="showLibrary" :showWishList="showWishList"></nav-bar>
   <!-- App -->
-  <div class="container-lg p-4 d-flex flex-wrap justify-content-around">
+  <div v-if="!isWishListVisible" class="container-lg p-4 d-flex flex-wrap justify-content-around">
     <book-card :book="book" v-for="book in books" :key="book.id"></book-card>
+  </div>
+
+  <div vi-if="isWishListVisible">
+    <h1>Wishlist Works !!!</h1>
   </div>
 </template>
 
 <script>
 import books from './assets/books.json'
 import BookCard from './components/BookCard.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
-  components: { BookCard },
+  components: { BookCard, NavBar },
   data() {
     return {
-      books: books
+      books: books,
+      isWishListVisible: false
+    }
+  },
+  methods: {
+    showLibrary() {
+      console.log('show library')
+      this.isWishListVisible = false
+    },
+    showWishList() {
+      console.log('show wishlist')
+      this.isWishListVisible = true
     }
   }
 }
---
+++ day-2/WishListUsingComponents/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" @click="showLibrary()">Library</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" @click="showWishList()">WishList</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    showLibrary: {
+      type: Function,
+      required: true
+    },
+    showWishList: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
