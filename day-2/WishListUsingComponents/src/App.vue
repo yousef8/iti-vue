d5d52e8e6becd50d509ec420b45d4ee93c0db753
Yousef Elsayed <yousef4511@outlook.com>
2024-04-20 13:24:18 +0200

    Refactor: Remove unnecessary imports & move things around

--- day-2/WishListUsingComponents/src/App.vue
+++ day-2/WishListUsingComponents/src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <nav-bar :showLibrary="showLibrary" :showWishList="showWishList"></nav-bar>
   <BookLibrary v-if="!isWishListVisible" />
-  <div v-if="isWishListVisible" class="container-lg p-4 d-flex flex-wrap justify-content-around">
-    <wish-list></wish-list>
-  </div>
+  <wish-list v-if="isWishListVisible"></wish-list>
 </template>
 
 <script>
-import books from './assets/books.json'
 import NavBar from './components/NavBar.vue'
 import WishList from './pages/WishList.vue'
 import BookLibrary from './pages/BookLibrary.vue'
-import { useWishesStore } from './stores/wishesStore'
 
 export default {
   components: { NavBar, WishList, BookLibrary },
   data() {
     return {
-      books: books,
       isWishListVisible: false
-    }
-  },
-  computed: {
-    wishesStore() {
-      return useWishesStore()
     }
   },
   methods: {
--- day-2/WishListUsingComponents/src/pages/WishList.vue
+++ day-2/WishListUsingComponents/src/pages/WishList.vue
@@ -1,25 +1,27 @@
 <template>
-  <table class="table">
-    <thead>
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Title</th>
-        <th scope="col">Author</th>
-        <th scope="col">Category</th>
-        <th scope="col">Price</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="book in wishesStore.wishes" :key="book.id">
-        <td>{{ book.id }}</td>
-        <td>{{ book.title }}</td>
-        <td>{{ book.author }}</td>
-        <td>{{ book.category }}</td>
-        <td>{{ book.price }}</td>
-        <td><a @click="wishesStore.removeFromWishes(book.id)">Remove</a></td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="container-lg p-4 d-flex flex-wrap justify-content-around">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Title</th>
+          <th scope="col">Author</th>
+          <th scope="col">Category</th>
+          <th scope="col">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="book in wishesStore.wishes" :key="book.id">
+          <td>{{ book.id }}</td>
+          <td>{{ book.title }}</td>
+          <td>{{ book.author }}</td>
+          <td>{{ book.category }}</td>
+          <td>{{ book.price }}</td>
+          <td><a @click="wishesStore.removeFromWishes(book.id)">Remove</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
